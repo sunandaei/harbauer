d81d5e8d1f95a5188daca68a08e3c1c60b32ad36
mrphpguru <mrphpguru@gmail.com>
2024-05-31 21:59:45 +0530

    changes

--- config/database.php
+++ config/database.php
@@ -32,16 +32,15 @@ return [
     */
 
     'connections' => [
-
-        'mongodb' => [
-            'driver'   => 'mongodb',
-            'host'     => env('MONGO_DB_HOST', '10.176.41.150'),
-            'port'     => env('MONGO_DB_PORT', 27017),
-            'database' => env('MONGO_DB_DATABASE', 'thetaDelta'),
-            'username' => env('MONGO_DB_USERNAME', 'phedroutineon'),
-            'password' => env('MONGO_DB_PASSWORD','S@9nDA5v4gh5#'),
-            'options'  => ['database' => 'thetaDelta']
-        ],
+            'mongodb' => [
+                    'driver'   => 'mongodb',
+                    'host'     => env('MONGO_DB_HOST', 'localhost'),
+                    'port'     => env('MONGO_DB_PORT', 27017),
+                    'database' => env('MONGO_DB_DATABASE', 'harbauer'),
+                    'username' => env('MONGO_DB_USERNAME'),
+                    'password' => env('MONGO_DB_PASSWORD'),
+                    'options'  => []
+                ],
     
         'sqlite' => [
             'driver' => 'sqlite',
